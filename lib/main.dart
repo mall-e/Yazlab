a97be81aac2f274a064fe278fbe231ab6a0a9790
mall-e <muhammedaliali25@gmail.com>
2023-10-17 16:16:12 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:postgres/postgres.dart';
 
-void main() {
+final connection = PostgreSQLConnection(
+  'localhost', // Sunucu adresi
+  5432,       // Port numarası
+  'yazlab', // Veritabanı adı
+  username: 'postgres',
+  password: 'efmukl123',
+);
+
+
+void main() async {
+
   runApp(const MyApp());
+
+  await connection.open();
+
+    PostgreSQLResult results = await connection.query('SELECT * FROM ogrenciler');
+    for (final row in results) {
+        print('ID: ${row[0]}, Ad: ${row[1]}');
+    }
+
+    await connection.close();
 }
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
